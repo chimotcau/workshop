9e29c6bf4537dcddcf32f018415ac62fc5d8fa43
вонгуендангхоа <129539631+chimotcau@users.noreply.github.com>
2025-11-24 15:07:14 +0300

    update bot

--- bot.py
+++ bot.py
@@ -1,9 +1,32 @@
 import random
 from engine_core import WHITE, BLACK
 
-class RandomBot:
+class SmartBot:
     def __init__(self, color):
         self.color = color
+
+    def evaluate_move(self, board, move):
+        
+        score = 0
+        fr_r, fr_c = move.fr
+        to_r, to_c = move.to
+        piece = board.piece_at(fr_r, fr_c)
+
+        score += 10 * len(move.captures)
+        
+        if piece and not piece.king:
+            if self.color == WHITE and to_r == 0:
+                score += 5
+            elif self.color == BLACK and to_r == board.size - 1:
+                score += 5
+
+        if piece and not piece.king:
+            if self.color == WHITE:
+                score += fr_r - to_r  
+            else:
+                score += to_r - fr_r  
+
+        return score
 
     def choose_move(self, board):
         all_moves = []
@@ -12,6 +35,12 @@ class RandomBot:
                 p = board.piece_at(r, c)
                 if p and p.color == self.color:
                     all_moves += board._piece_moves(r, c, p)
+
         if not all_moves:
             return None
-        return random.choice(all_moves)
+
+        scored_moves = [(self.evaluate_move(board, m), m) for m in all_moves]
+        max_score = max(score for score, _ in scored_moves)
+        best_moves = [m for score, m in scored_moves if score == max_score]
+
+        return random.choice(best_moves)
